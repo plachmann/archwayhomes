3cdad6951bddbe66a894fb8576d39c5548d82377
Phil Lachmann <phil.lachmann@gmail.com>
2017-05-14 09:51:19 -0400

    carosel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICarouselConfig, AnimationConfig } from 'angular4-carousel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  public imageSources: string[];
+
+  public config: ICarouselConfig = {
+    verifyBeforeLoad: false,
+    log: true,
+    animation: true,
+    animationType: AnimationConfig.SLIDE,
+    autoplay: true,
+    autoplayDelay: 2000,
+    stopAutoplayMinWidth: 768
+  };
+
+  constructor() {}
+
+ ngOnInit(){
+     // called after the constructor and called  after the first ngOnChanges()
+    
+     this.imageSources = ['assets/images/Aiden-0.png'
+                         ];
+
+      console.log(this.imageSources);
+
+       console.log(this.config);
+  }
+
+
 }
